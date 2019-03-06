f897690d8c6c5343cc265b6299766807abf4292b
Kyle Barth <40353515+kyle-barth@users.noreply.github.com>
2019-03-06 20:56:12 +0000

    refactored code for sound to be non-blocking

--- screm.py
+++ screm.py
@@ -3,37 +3,47 @@ import threading
 import serial
 import random
 import os
+import time
 
+# define audio player and # of channels & path for audio files
 pygame.mixer.init()
+pygame.mixer.set_num_channels(100)
+path = os.getcwd().replace("\\", "/") + "/audio/"
 
-class myThread (threading.Thread):
-    def __init__(self, audioFile):
-        threading.Thread.__init__(self)
-        self.audioFile = audioFile
-    def run(self):
-        arduinoSerialData.flushInput()
-        pygame.mixer.music.load(audioFile)
-        pygame.mixer.music.play()
-
-# Define the IO serial data
+# define the IO serial data 
 arduinoSerialData = serial.Serial('/dev/ttyACM0', 9600)
-audioPath = os.getcwd().replace("\\", "/") + "/audio/"
 
-# Create new threads
-thread1 = myThread('')
-thread2 = myThread('')
+# range the bot will react, in cm
+territory = 20
+# minimum volume the bot will play (from 0.0 -> 1.0)
+minVolume = 0.2
+# how long the bot pauses between sounds
+wait = 0.1
 
 while True :
-    if(thread1.isAlive() == False or thread2.isAlive() == False) :
-        if (arduinoSerialData.inWaiting()>0) :
-            if (arduinoSerialData.readline()) :
-                distance = int(arduinoSerialData.readline().decode('utf8'))
-                if (distance < 20) :
-                    audioFile = audioPath+random.choice(os.listdir(audioPath))
-                    print(audioFile)
-                    if(thread1.isAlive() == False) :
-                        thread1 = myThread(audioFile)
-                        thread1.start()
-                    elif(thread2.isAlive() == False) :
-                        thread2 = myThread(audioFile)
-                        thread2.start()
+    if (arduinoSerialData.inWaiting()>0) :
+        if (arduinoSerialData.readline()) :
+            distance = int(arduinoSerialData.readline().decode('utf8'))
+            
+            if (distance < territory) :
+                print('Something in range within', territory, ' cm!')
+                
+                file = path+random.choice(os.listdir(path))
+                print('sound: '+file.split('/audio/',1)[1])
+                
+                absoluteVolume = str(1-distance/territory)
+                volume = float(absoluteVolume[:absoluteVolume.find('.')+2])
+                if (volume < minVolume) :
+                    volume = minVolume
+                print('volume: ', volume)
+                
+                audio = pygame.mixer.Sound(file)
+                audio.set_volume(volume)
+                if (pygame.mixer.find_channel()) :
+                    channel = pygame.mixer.find_channel()
+                    channel.queue(audio)
+                
+                print('')
+                
+    arduinoSerialData.flushInput()
+    time.sleep(wait)
